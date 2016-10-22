435bc15635b77a10eee217e844d3789d1cd3b4f9
Sameer87 <direct2sameer@gmail.com>
2016-10-22 13:22:58 +0530

    models.py updated

--- storesmart/ware/models.py
+++ storesmart/ware/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Userform(models.Model):
+    user = models.OneToOneField(User)
+    flag = models.PositiveSmallIntegerField(default=0)
+
+    def __str__(self):
+        return self.user.username
+
+
+class warehouse(models.Model):
+	user=models.CharField(max_length=200)
+	location = models.CharField(max_length=1000)
+	cold_rate=models.PositiveSmallIntegerField(default=0 ,null=True)
+	hot_rate=models.PositiveSmallIntegerField(default=0 ,null=True)
+	mild_rate=models.PositiveSmallIntegerField(default=0 ,null=True)
+	severe_rate=models.PositiveSmallIntegerField(default=0 ,null=True)
+	cold_total=models.PositiveSmallIntegerField(default=0 ,null=True)
+	hot_total=models.PositiveSmallIntegerField(default=0 ,null=True)
+	mild_total=models.PositiveSmallIntegerField(default=0 ,null=True)
+	severe_total=models.PositiveSmallIntegerField(default=0 ,null=True)
+	cold_available=models.PositiveSmallIntegerField(default=0 ,null=True)
+	hot_available=models.PositiveSmallIntegerField(default=0 ,null=True)
+	mild_available=models.PositiveSmallIntegerField(default=0 ,null=True)
+	severe_available=models.PositiveSmallIntegerField(default=0 ,null=True)
+
+	def __str__(self):
+		return self.location
+
+class order(models.Model):
+	ware_owner=models.CharField(max_length=200)
+	user = models.CharField(max_length=200)
+	quantity = models.PositiveSmallIntegerField(default=0,null=True )
+	type_of=models.CharField(max_length=100)
+	price= models.PositiveSmallIntegerField(default=0,null=True )
+
+	def __str__(self):
+		return self.ware_owner + " - " + self.user
+
+		
+    
+
+
